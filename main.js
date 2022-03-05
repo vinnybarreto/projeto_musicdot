2fb4af427dce5e192c0c79746b9a0594f23a1430
Vinicius Alves <vinnybarreto@gmail.com>
2022-03-05 20:26:25 +0000

    update

--- main.js
+++ main.js
@@ -1,16 +1,50 @@
 //Declaração de variáveis
-var toggleMenu = document.querySelectorAll(".toggle_menu")
-var buttonMenu = document.querySelector(".menu_btn")
-
-
+var toggleMenu = document.querySelectorAll(".toggle_menu") //Variável recebe classe do toggle
+var menuMobile = document.querySelector('.menu_mob') //Variável recebe classe do menu
+var buttonMenu = document.querySelector(".menu_btn ion-icon")
 
 //Abrir e fechar o menu mobile
-for(var n=0; n < toggleMenu.length; n++){
-    toggleMenu[n].addEventListener('click', function(){
+for (var n = 0; n < toggleMenu.length; n++) {
+    toggleMenu[n].addEventListener('click', function () {
         var overlay = document.querySelector('.overlay_mob');
-        overlay.classList.toggle('.overlay_open');
+        overlay.classList.toggle('overlay_open');
+        menuMobile.classList.toggle('menu_mob_close');
+        menuMobile.classList.toggle('menu_mob_open');
 
-    })
+        var icon = buttonMenu.getAttribute('name') //variavel icon receber o atributo name da classe menu_btn
+
+        if (icon === 'menu'){
+            buttonMenu.setAttribute('name', 'close');//Se está como menu altera para close
+        }
+        else{
+            buttonMenu.setAttribute('name', 'menu'); //Se está como close altera para menu
+        }
+    });
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
